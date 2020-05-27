c6deeea2702e9fdcfab37ee485d02bc9af3ca7d2
R. Ito <co.panda.fc3s@gmail.com>
2020-05-27 20:07:41 +0900

    ちゃんとアップロードできるようにした

--- gsheet.py
+++ gsheet.py
@@ -1,32 +1,29 @@
 # -*- coding:utf-8 -*-
-from __future__ import print_function
-import pickle
-import os.path
+import os
+import google.oauth2.credentials
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+def get_auth():
+  credentials = google.oauth2.credentials.Credentials(
+      os.environ.get('ACCESS_TOKEN'),
+      refresh_token=os.environ.get('REFRESH_TOKEN'),
+      token_uri='https://oauth2.googleapis.com/token',
+      client_id=os.environ.get('CLIENT_ID'),
+      client_secret=os.environ.get('CLIENT_SECRET')
+    )
 
-def get_authentication():
-  creds = None
-  # The file token.pickle stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-      creds = pickle.load(token)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-        'credentials.json', SCOPES)
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-      pickle.dump(creds, token)
-
-  service = build('sheets', 'v4', credentials=creds)
+  service = build('sheets', 'v4', credentials=credentials)
   return service
+
+def write_data(spreadsheet_id, values, service, range):
+  body = {
+    'values': [values],
+  }
+  service.spreadsheets().values().append(
+    spreadsheetId=spreadsheet_id,
+    range=range,
+    valueInputOption='USER_ENTERED',
+    body=body
+  ).execute()
+
